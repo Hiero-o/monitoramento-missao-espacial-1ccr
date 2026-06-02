f8acef188107346b3f1a45dbdf95e868638394b4
Gustavo Araujo Ramos da Silva <gustavoardss@gmail.com>
2026-06-01 23:37:50 -0300

    Corrigindo lógica do programa

--- main.c
+++ main.c
@@ -1,60 +1,17 @@
 #include<stdlib.h>
 #include<stdio.h>
 char nome[25];
-int temp, opcao;
-float energia, status;
+int temp, opcao, comunicacao;
+float energia;
 
-int main(){
-    printf("="*10);
-    printf("MENU");
-    printf("="*10);
-
-    printf("1 - Inserir dados\n");
-    printf("2 - Visualizar status\n");
-    printf("3 - Executar análise\n");
-    printf("4 - Encerrar sistema\n");
-
-    scanf("%d", &opcao);
-
-    // Confere se o usuário já inseriu dados anteriormente.
-
-    while opcao != 1 {
-
-        printf("Por favor, insira os dados antes de acessar as outras opções.\n");
-        printf("Digite 1 para inserir os dados:\n");
-
-        scanf("%d", &opcao);
-    }
-
-
-    switch(opcao){
-        case 1:
-            insere_dados():
-            continue;
-
-        case 2:
-            status();
-            continue;
-
-        case 3:
-            executa_analise();
-            continue;
-
-        case 4:
-            printf("Sessão finalizada!")
-            break;
-
-    }
-
-}
 
 void insere_dados(){
-    printf("=" * 60);
+    printf("=======================================");
     printf("Olá, seja bem-vindo ao centro de monitoramento da nave!");
-    printf("=" * 60);
+    printf("=======================================");
     
     printf("\n\nDigite o seu primeiro nome:\n");
-    scanf("%s", &nome);
+    scanf("%s", nome);
     printf("\n Olá, %s!, prosseguiremos com o monitoramento da nave.\n", nome);
     
     printf("\n\nDigite a temperatura atual da nave:\n");
@@ -65,37 +22,45 @@ void insere_dados(){
     scanf("%f", &energia);
     
     printf("\nDigite o Status da comunicação\n");
-    printf("| BOM | REGULAR | RUIM |\n");
-    scanf("%d", &status);
+    printf("| 0 | 10 | 20 | 30 | 40 | 50 | 60 | 70 | 80 | 90 | 100 |\n");
+    scanf("%d", &comunicacao);
     
     
 }
 
 void analise() {
+    condicoes();
     
 }
 
 void status (){
+    printf("=======================================");
+    printf("Status da Missão");
+    printf("=======================================");
+
+    printf("\n\n Temperatura atual da nave: %d", temp);
+    printf("\n\n A energia está em %.2f%%", energia);
+    printf("\n\n A qualidade da comunicação está em: %d%%", comunicacao);
 
 }
 
 void condicoes() {
-
+    
     // verificando condições de temperatura
-
-    if temp > 100{
+    
+    if (temp > 100){
         printf("\nA nave estrará em estado crítico! Risco de falha total dos sistemas!\n");
-
-    } if else temp > 80 and temp <= 100 {
+        
+    } else if (temp > 80 && temp <= 100) {
         printf("\nAlerta de superaquecimento!\n");
 
-    } if else temp >=60 and temp <= 80 {
+    } else if (temp >=60 && temp <= 80) {
         printf("\nA temperatura da nave está dentro dos limites seguros.\n");
-
-    } if else temp >= 20 and temp < 60 {
+        
+    } else if (temp >= 20 && temp < 60) {
         printf("\nA temperatura da nave está em níveis recomendados.\n");
-
-    } if else temp < 20 {
+        
+    } else if (temp < 20) {
         printf("\nA temperatura da nave está muito baixa, risco de dano aos sistemas!\n");
         
     } else {
@@ -103,36 +68,91 @@ void condicoes() {
     }
 
     // Verificando condições de energia
-
-    if energia < 20 {
+    
+    if (energia < 20) {
         printf("\nA nave entrará em modo de economia de energia.\n Recarregue a nave!\n");
-
-    } if else energia >= 20 and energia <= 50 {
+        
+    } else if (energia >= 20 && energia <= 50) {
         printf("\nA nave está operando abaixo da capacidade recomendada.\n Recomenda-se o abastecimento de energia");
-
-    }if else energia >50 and energia <=75 {
+        
+    }else if (energia >50 && energia <=75) {
         printf("\nA nave está operando com níveis seguros de energia");
-
+        
     } else {
         printf("\nA nave está operando com capacidade máxima\n");
     }
-
+    
     // verificando condições de comunicação
-
-    if status == 0 {
+    
+    if (comunicacao == 0) {
         printf("Sem condições de comunicação.");
-
-    } if else status > 0 and status <= 25 {
+        
+    } else if (comunicacao > 0 && comunicacao <= 25) {
         printf("\nAs condições de comunicação foram severamente prejudicadas.\n");
-
-    } if else status > 25 and status <= 50 {
+        
+    } else if (comunicacao > 25 && comunicacao <= 50) {
         printf("\nAs condições de comunicação estão prejudicadas, porém, funcionando\n");
 
-    } if else status > 50 and status <= 75 {
+    } else if (comunicacao > 50 && comunicacao <= 75) {
         printf("\n As condições de comunicação estão em bom estado.\n");
 
     } else {
-        printf("\nAs condições de comunicação estão em ótimo estado.\n")
+        printf("\nAs condições de comunicação estão em ótimo estado.\n");
     }
+    
+}
 
+
+int main(){
+
+    int dados_inseridos = 0;
+
+    while(opcao != 4){
+        
+        printf("=======================================");
+        
+        printf("MENU");
+
+        printf("=======================================");
+        
+        printf("1 - Inserir dados\n");
+        printf("2 - Visualizar status\n");
+        printf("3 - Executar análise\n");
+        printf("4 - Encerrar sistema\n");
+        
+        scanf("%d", &opcao);
+
+
+        switch(opcao){
+            case 1:
+                insere_dados();
+                dados_inseridos = 1;
+                break;
+                
+
+            case 2:
+            if(dados_inseridos == 0){
+                printf("Por favor, insira os dados primeiro.");
+            }
+                status();
+                break;
+                
+
+            case 3:
+            if(dados_inseridos == 0){
+                printf("Por favor, insira os dados primeiros.");
+            }
+                analise();
+                break;
+                
+
+            case 4:
+                printf("Sessão finalizada!");
+                break;
+
+            default:
+                printf("Opção inválida, por favor, escolha uma opção válida.");
+
+        }
+    }
 }
